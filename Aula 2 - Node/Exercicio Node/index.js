965c6b7a86b8c0e8ec2935da9d2f220d534153ba
Henrique Vidal <henriqueevidal@hotmail.com>
2023-05-05 22:46:20 -0300

    Adiciona linha de exibição de mensagem que indica o proposito da modificação

--- Aula 2 - Node/Exercicio Node/index.js
+++ Aula 2 - Node/Exercicio Node/index.js
@@ -3,6 +3,8 @@ const prompt = require('prompt-sync')()
 let salario = Number(prompt('Insira o seu salário: '))
 let percentual = Number(prompt('Insira o percentual do reajuste: '))
 
-let reajuste = salario*(1+(percentual/100))
+let reajuste = salario * (1 + (percentual / 100))
 
-console.log("O valor do salario reajustado é de: "+reajuste)
+console.log("O valor do salario reajustado é de: " + reajuste)
+
+console.log("Adicionando para atividade de GIT")
